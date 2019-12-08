44f30a06f52d2630ef152e57332a74d739b183bb
Syelkonya <55093905+Syelkonya@users.noreply.github.com>
2019-12-08 12:42:29 +0300

    hw after 051219, try rebuild arrow

--- app/src/main/java/com/example/speedometer/SpeedometerView.java
+++ app/src/main/java/com/example/speedometer/SpeedometerView.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Shader;
@@ -18,13 +19,12 @@ import androidx.annotation.Nullable;
 
 public class SpeedometerView extends View {
 
-    private static final String TAG ="Speedometer";
+    private static final String TAG = "Speedometer";
     private static final Paint ARC_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final Paint TEXT_MIN_VELOCITY_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final Paint TEXT_MAX_VELOCITY_PAINT = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final Paint CENTRAL_NET = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final Paint TEXT_VELOCITY = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private static final Paint ARROW = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final float STROKE_WIDTH = 50f;
     private static final float START_ANGLE = 160f;
     private static float RADIUS = 300f;
@@ -42,6 +42,8 @@ public class SpeedometerView extends View {
     private Rect mTextBounds = new Rect();
     private int mProgress;
     private RectF mArcRect;
+    private Paint arrow = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private Path mPath = new Path();
 
 
     public int getProgress() {
@@ -58,7 +60,7 @@ public class SpeedometerView extends View {
     }
 
     public SpeedometerView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public SpeedometerView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
@@ -69,9 +71,13 @@ public class SpeedometerView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawArc(mArcRect, START_ANGLE, END_ANGLE, false, ARC_PAINT);
-        canvas.drawCircle(mArcRect.width()/2, mArcRect.height()/2, NET_RADIUS, CENTRAL_NET);
+        canvas.drawCircle(mArcRect.width() / 2, mArcRect.height() / 2, NET_RADIUS, CENTRAL_NET);
         drawText(canvas);
-        drawLine(canvas);
+        drawArrow(canvas);
+        float angle = mProgress + START_ANGLE;
+        canvas.translate(mArcRect.width() / 2f, mArcRect.height() / 2f);
+        canvas.rotate(angle);
+        canvas.drawPath(mPath, arrow);
     }
 
     @Override
@@ -92,42 +98,32 @@ public class SpeedometerView extends View {
         mArcRect = new RectF(getPaddingLeft() + STROKE_WIDTH / 2, STROKE_WIDTH / 2 + getPaddingTop(), size - STROKE_WIDTH / 2 - getPaddingRight(), size - STROKE_WIDTH / 2 - getPaddingBottom());
     }
 
-    private void drawLine(Canvas canvas) {
-        float angle = (float) (Math.PI * (mProgress + START_ANGLE) / 180 );
-        float lengthOfArrow = (float) (mArcRect.width()/2*0.7);
-        float x = (float) (mArcRect.width()/2+ lengthOfArrow * Math.cos(angle) + mTextBounds.width());
-        float y = (float) (mArcRect.height()/2 + lengthOfArrow * Math.sin(angle));
-        Pt[] myPath;
-        if (mProgress>60 && mProgress<150){
-            myPath = new Pt[]{new Pt(x, y), new Pt(mArcRect.width()/2 + NET_RADIUS * 0.25f, mArcRect.height()/2 ),
-                    new Pt(mArcRect.width()/2 - NET_RADIUS * 0.25f, mArcRect.height()/2), new Pt(x, y)};
-        }else {
-            myPath = new Pt[]{new Pt(x, y), new Pt(mArcRect.width()/2, (mArcRect.height()/2 + NET_RADIUS * 0.25f)),
-                    new Pt(mArcRect.width()/2, mArcRect.height()/2 - NET_RADIUS * 0.25f), new Pt(x, y)};
-        }
-        Path path = new Path();
-        path.moveTo(myPath[0].x, myPath[0].y);
-        for (int i = 1; i < myPath.length; i++){
-            path.lineTo(myPath[i].x, myPath[i].y);
-        }
-        canvas.drawPath(path, ARROW);
+    private void drawArrow(Canvas canvas) {
+        Point a = new Point(0, -16);
+        Point b = new Point(0, 16);
+        Point c = new Point(300, 0);
+
+        mPath.setFillType(Path.FillType.EVEN_ODD);
+        mPath.lineTo(b.x, b.y);
+        mPath.lineTo(c.x, c.y);
+        mPath.lineTo(a.x, a.y);
+        mPath.close();
     }
 
     private void drawText(Canvas canvas) {
-        float xForMin = (float) (mArcRect.width()/2 - mArcRect.width()/2 * Math.sin(20));
-        float y = (float) (mArcRect.height()/2 + mArcRect.height()/2 * Math.cos(20)) + FONT_MIN_SIZE;
+        float xForMin = (float) (mArcRect.width() / 2 - mArcRect.width() / 2 * Math.sin(20));
+        float y = (float) (mArcRect.height() / 2 + mArcRect.height() / 2 * Math.cos(20)) + FONT_MIN_SIZE;
         canvas.drawText("0 км/ч", xForMin, y, TEXT_MIN_VELOCITY_PAINT);
 
-        float xForMax = (float) (mArcRect.width()/2 + mArcRect.width()/2 * Math.sin(20) - 2*FONT_MIN_SIZE);
+        float xForMax = (float) (mArcRect.width() / 2 + mArcRect.width() / 2 * Math.sin(20) - 2 * FONT_MIN_SIZE);
         canvas.drawText("220 км/ч", xForMax, y, TEXT_MAX_VELOCITY_PAINT);
 
         final String progressString = formatString(mProgress);
         float xForV = mArcRect.width() / 3f - mTextBounds.width() / 2f - mTextBounds.left;
         float yForV = mArcRect.height() / 2f + mTextBounds.height() / 2f - mTextBounds.bottom +
-                mArcRect.top +(float) Math.PI*NET_RADIUS;
+                mArcRect.top + (float) Math.PI * NET_RADIUS;
         canvas.drawText(progressString, xForV, yForV, TEXT_VELOCITY);
     }
-
 
 
     private String formatString(int progress) {
@@ -143,8 +139,8 @@ public class SpeedometerView extends View {
 
 
     private void configureArrow() {
-        ARROW.setColor(Color.GRAY);
-        ARROW.setStyle(Paint.Style.FILL);
+        arrow.setColor(Color.GRAY);
+        arrow.setStyle(Paint.Style.FILL);
     }
 
 
@@ -160,8 +156,8 @@ public class SpeedometerView extends View {
         ARC_PAINT.setStyle(Paint.Style.STROKE);
         ARC_PAINT.setStrokeWidth(STROKE_WIDTH);
 //        RADIUS = mArcRect.width()/2;
-        shaderGradient = new LinearGradient(0, RADIUS, 2f *RADIUS, RADIUS,
-                new int[] { Color.GREEN, Color.YELLOW, Color.YELLOW, Color.RED }, null, Shader.TileMode.CLAMP);
+        shaderGradient = new LinearGradient(0, RADIUS, 4f * RADIUS, RADIUS,
+                new int[]{Color.GREEN, Color.YELLOW, Color.YELLOW, Color.RED}, null, Shader.TileMode.CLAMP);
         ARC_PAINT.setShader(shaderGradient);
     }
 
@@ -173,10 +169,4 @@ public class SpeedometerView extends View {
 
 }
 
-class Pt{
-    float x, y;
-    Pt(float x, float y){
-        this.x = x;
-        this.y = y;
-    }
-}
+
